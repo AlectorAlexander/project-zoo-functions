4e53771bdee29df94489b3fc405ff746374c2ea8
Aléctor Alexander <alector.alexandre@gmail.com>
2022-01-27 17:43:51 -0300

    linit2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,6 +1,6 @@
 // const { animal } = require('faker/lib/locales/en');
 const data = require('../data/zoo_data');
-
+// Referência q me ajudou a fazer esse exercício: Fernanda Ferreira - Turma 19 - Tribo B - Slack
 function getAnimalsOlderThan(animal, Fage) {
   const { species } = data;
   const animais = species.find(({ name }) => name === animal);
