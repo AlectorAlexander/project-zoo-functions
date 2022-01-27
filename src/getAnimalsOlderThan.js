2cc4201b0e867041c21cfe7bc6f340a4232e09f1
Aléctor Alexander <alector.alexandre@gmail.com>
2022-01-27 16:09:18 -0300

    requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,11 @@
+// const { animal } = require('faker/lib/locales/en');
 const data = require('../data/zoo_data');
 
-function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+function getAnimalsOlderThan(animal, Fage) {
+  const { species } = data;
+  const animais = species.find(({ name }) => name === animal);
+  return animais.residents.every(({ age }) => age >= Fage);
 }
-
+const actual = getAnimalsOlderThan('otters', 7);
+console.log(actual);
 module.exports = getAnimalsOlderThan;
