7ad5f1da1c39eb42e5478af65ceebae6fb8f44f3
Aléctor Alexander <alector.alexandre@gmail.com>
2022-01-27 16:57:15 -0300

    requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,17 @@
 const data = require('../data/zoo_data');
+// Referência: Queite  Castiglioni / Turmar 19 - Tribo A me ajudou no Zoom;
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
-}
+  const { employees } = data;
+  if (employeeName === undefined) {
+    return {};
+  }
+  const verifyName = employees.find(({ firstName }) => firstName === employeeName);
+  const verifyLastName = employees.find(({ lastName }) => lastName === employeeName);
 
+  if (verifyName === undefined) {
+    return verifyLastName;
+  }
+  return verifyName;
+}
 module.exports = getEmployeeByName;
