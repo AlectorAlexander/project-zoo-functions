df4f43e239c3c9c87c6043a548a56170440a8bc5
Aléctor Alexander <alector.alexandre@gmail.com>
2022-02-03 11:35:06 -0300

    80%

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,15 +1,22 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+const array = [];
+employees.forEach((element) => element.managers.map((gerente) => array.push(gerente)));
+
 function isManager(id) {
-  // seu código aqui
+  return array.some((managers) => (managers === id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId)) {
+    return employees.filter((element) => element.managers.includes(managerId))
+      .map((element) => `${element.firstName} ${element.lastName}`);
+    // const employs = employees.find((element) => element.id === managerId).responsibleFor;
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
 
-module.exports = { isManager, getRelatedEmployees };
+console.log(getRelatedEmployees('9e7d4524-363c-416a-8759-8aa7e50c0992'));
 
-const algo = data.employees;
-// const filtro = (manager) => algo.filter([manager]);
-algo.forEach((employers, current) => console.log(employers));
+module.exports = { isManager, getRelatedEmployees };
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -6,5 +6,6 @@ function getSpeciesByIds(...ids) {
   }
   return ids.map((element) => data.species.find((species) => species.id === element));
 }
+console.log(getSpeciesByIds('01422318-ca2d-46b8-b66c-3e9e188244ed'));
 
 module.exports = getSpeciesByIds;
